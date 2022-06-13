888eb8a7963b3f6b8043dd1ff43d07a84150d485
sun <sun@codebrick.co>
2022-06-14 02:52:05 +0900

    Add animator

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,10 +9,12 @@ public class PlayerMovement : MonoBehaviour
 
     Vector2 moveInput;
     Rigidbody2D rigidBody;
+    Animator animator;
 
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -31,12 +33,15 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 playerVelocity = new Vector2 (moveInput.x * speed, 0);
         rigidBody.velocity = playerVelocity;
+        bool hasHorizontalSpeed = Mathf.Abs(rigidBody.velocity.x) > Mathf.Epsilon;
+        // Mathf.Abs는 절대값을 반환하고, Mathf.Epsilon은 0에 가까운 수인데 그냥 0이라고 생각해도 무방하다.
+
+        animator.SetBool("isRunning", hasHorizontalSpeed);
     }
 
     void FlipSprite()
     {
         bool hasHorizontalSpeed = Mathf.Abs(rigidBody.velocity.x) > Mathf.Epsilon;
-        // Mathf.Abs는 절대값을 반환하고, Mathf.Epsilon은 0에 가까운 수인데 그냥 0이라고 생각해도 무방하다.
 
         if (hasHorizontalSpeed)
         {
