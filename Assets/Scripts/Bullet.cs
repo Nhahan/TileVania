173ab4f28a8fa20e0e7cfd7c62a47d4aea1d3933
sun <sun@codebrick.co>
2022-06-16 03:38:00 +0900

    Add attacking enemy with bullets

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rigidBody;
+    PlayerMovement player;
+    float bulletSpeed;
+
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
+        player = FindObjectOfType<PlayerMovement>();
+        bulletSpeed = player.transform.localScale.x * 5f;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        rigidBody.velocity = new Vector2 (bulletSpeed, 0);
+        transform.Rotate(0, 0, -70 * 1.25f * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+        }
+        if (other.tag != "Player" && other.tag != "Bullet")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    float GenerateFloat()
+    {
+        System.Random rnd = new System.Random();
+        return (float)rnd.Next(-2, 2) + 1f;
     }
 }
